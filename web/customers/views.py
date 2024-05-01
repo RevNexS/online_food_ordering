cde22f9cb2cb1d62be6f019bb650ae997228dc8a
Vi1512kas <111676209+Vi1512kas@users.noreply.github.com>
2024-05-02 00:06:13 +0530

    Update views.py

--- web/customers/views.py
+++ web/customers/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def customer_signup(request):
+    customers = Customers.object.all()
+    return render(request, 'customer_registration.html', {'customers':customers})
