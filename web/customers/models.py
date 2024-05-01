72c0d04d659e3ea83a928ab7f82194c69f9a1837
Vi1512kas <111676209+Vi1512kas@users.noreply.github.com>
2024-05-02 00:05:28 +0530

    Update models.py

--- web/customers/models.py
+++ web/customers/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Customers(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128, verbose_name='password', help_text='USE A STRONG PASSWORD')
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "".format(self.first_name, self.last_name)
+
+
+class Address(models.Model):
+    street_address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.street_address}, {self.city},{self.postal_code}"
+
