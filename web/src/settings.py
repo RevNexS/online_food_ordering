64a23f5af5bd9f8a40bf9fcc2c94f9d7a6591586
Sahil <choudharysahil47@gmail.com>
2024-05-30 16:48:57 +0530

    serving hero page from backend server
    hero.html is now being serve by django. CSS import was fixed. All static files was collected and default STATIC_ROOT was changed to be server outside /web folder. New img folder was created to serve all images from single folder how. Images can now be have src simply as /static/img/image.jpg instead of being used static tag. Though static tag can also be used.
    
    Next : we have to show all registered resto on this page who have sign up. Register Resto can upload images of their resto and images of the menu. That has to be dinamically show on menu pages.

--- web/src/settings.py
+++ web/src/settings.py
@@ -75,9 +75,6 @@ STORAGES = {
     },
 }
 
-# Static files
-
-STATIC_ROOT = BASE_DIR / "static"
 
 WSGI_APPLICATION = 'src.wsgi.application'
 
@@ -129,7 +126,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# Changed default static folder to be outside /web.
+STATIC_ROOT = BASE_DIR / "../static" # storing static files outside project. 
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    # BASE_DIR / "static",
+    # BASE_DIR / "../static"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
